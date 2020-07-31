12fbbf4e4f371d5b8867f77a447fb29d2795c81d
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-07-31 17:26:52 -0400

    Finish renaming cookbook and update metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,11 @@
 name 'ros_buildfarm'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Steven! Ragnarök'
+maintainer_email 'steven@openrobotics.org'
 license 'Apache-2.0'
-description 'Installs/Configures ros_buildfarm_deployment'
-long_description 'Installs/Configures ros_buildfarm_deployment'
+description 'Installs/Configures ros_buildfarm'
+long_description 'Installs/Configures ros_buildfarm'
 version '0.1.0'
-chef_version '>= 14.0'
+chef_version '>= 15.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/ros_buildfarm_deployment/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/ros_buildfarm_deployment'
+issues_url 'https://github.com/nuclearsandwich/cookbook-ros-buildfarm/issues'
+source_url 'https://github.com/nuclearsandwich/cookbook-ros-buildfarm/'
