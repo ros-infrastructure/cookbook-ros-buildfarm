c839b1cdf01e1018db023aee07df09321f7da29b
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-08-06 18:10:13 -0400

    Add host keys to data bag and repo recipe.

--- recipes/repo.rb
+++ recipes/repo.rb
@@ -1,6 +1,17 @@
 package "reprepro"
 
 package "openssh-server"
-# TODO set host keys from attributes / data bag.
+
+host_keys = data_bag_item("ros_buildfarm_host_private_keys", "repo.#{node.chef_environment}")
+%w(dsa ecdsa ed25519 rsa).each do |type|
+  file "/etc/ssh/ssh_host_#{type}_key" do
+    content host_keys[type]["private"]
+    mode "0600"
+  end
+  file "/etc/ssh/ssh_host_#{type}_key.pub" do
+    content host_keys[type]["public"]
+    mode "0644"
+  end
+end
 
 
