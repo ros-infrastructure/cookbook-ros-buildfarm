49430c575968d7268d8ddcd10fc30f36956ebaa0
Maanasa K <maanasa@openrobotics.org>
2020-08-12 12:14:45 -0700

    Added jenkins server recipe and modified things to work with it.
    The Berskfile was added for the supermarket jenkins cookbook dependency. The metadata file was changed for the same reason. Attributes were added to fulfill some that were referenced in the jenkins recipe. Kitchen.yml was modified to add the recipe to the run list.

---
+++ attributes/jenkins.rb
@@ -0,0 +1,2 @@
+default['ros_buildfarm']['jenkins']['admin']['key'] = ''
+default['ros_buildfarm']['jenkins']['timezone'] = ''
--- recipes/jenkins.rb
+++ recipes/jenkins.rb
@@ -1,38 +1,67 @@
-##JENKINS AGENT##
+##JENKINS SERVER##
   
-include_recipe 'jenkins::master' #do I include all the recipes on the cookbook and is that right syntax?
+include_recipe 'jenkins::master'
 
-include_recipe 'agent' #ros buildfarm agent recipe
-
-file '/var/jenkins/casc_configs/jenkins.yaml' do
+#rosplugins need to download plugin TODO
+#copy loop into recipe use node['ros_buildfarm']['jenkins_plugins']
+node['ros_buildfarm']['jenkins']['plugins'].each do |plugin, ver|
+  jenkins_plugin plugin do
+    version ver
+    install_deps false
+    notifies :restart, 'service[jenkins]', :delayed
+  end
 end
 
-jenkins::jenkins_user 'admin' do
-  public_keys node['jenkins']['admin']['key']
+include_recipe '::agent' #ros buildfarm agent recipe, TODO: change attributes in this recipe before including 
+
+#jenkins config as code do i need to do more like make the contents of the yaml file
+#copy configuration file from config file to template 
+template '/var/jenkins/casc_configs/jenkins.yaml' do
+  group node['jenkins']['master']['group']
+  source 'jenkins.yaml.erb' #idk what this file should be named
 end
 
-timezone 'PST'
+#idk if this is required but the plugin says this environement variable needs to be defined
+execute 'CASC_JENKINS_CONFIG' do
+  environment ({'CASC_JENKINS_CONFIG' => '/var/jenkins/casc_configs/jenkins.yaml'})
+end
+
+#user directories, attribute or variable here? not needed for now
+
+#authentication TODO
+jenkins_user 'admin' do
+  email 'todo@something.com'
+  public_keys [node['ros_buildfarm']['jenkins']['admin']['key']]
+end
+
+timezone node['ros_buildfarm']['jenkins']['timezone']
 
 package 'nginx'
+#Steven TODO 
+template '/etc/nginx/sites-enabled/jenkins.conf' do
+end
 service 'nginx' do
   action [ :enable, :start]
-end
-template 'path/to/some/file' do
 end
 
 package 'python3-yaml'
 
 package 'docker'
 
-jenkins_private_key_credentials 'idk' do
-  id 'agent'
-  description 'a slave machine'
-  private_key node['jenkins']['slave']['key']
+#data bag query and loop through it
+node['ros_buildfarm']['jenkins']['credentials'].each do |name, agent, des, key|
+  jenkins_private_key_credentials agent do
+    id name
+    description des
+    private_key key
+  end
 end
 
-jenkins_password_credentials 'idk' do
-  id node['jenkins']['admin']['username']
-  description 'credentials for jenkins?'
-  password node['jenkins']['admin']['password']
+#data bag query loop
+node['ros_buildfarm']['jenkins']['credentials'].each do |name, agent, des, pass|
+  jenkins_password_credentials agent do
+    id name
+    description des
+    password pass
+  end
 end
-
