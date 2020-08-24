c1112b2218f0228a46ea3530f2325df5b0ecc822
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-08-24 08:20:06 -0400

    Install acme.sh.

--- recipes/jenkins.rb
+++ recipes/jenkins.rb
@@ -109,6 +109,21 @@ end
 
 timezone node['ros_buildfarm']['jenkins']['timezone']
 
+# Install acme.sh for certificate signing and renewal.
+# git is required for acme.sh's setup
+package "git"
+execute "git clone https://github.com/acmesh-official/acme.sh" do
+  cwd "/root"
+  not_if "test -d /root/acme.sh"
+end
+execute "acmesh-install" do
+  cwd "/root/acme.sh"
+  cmd = "./acme.sh --install --home /root/.acme.sh"
+  cmd << " --accountemail #{node['ros_buildfarm']['letsencrypt_email']}" if node['ros_buildfarm']['letsencrypt_email']
+  command cmd
+  not_if "test -x /root/.acme.sh/acme.sh"
+end
+
 package 'nginx'
 #Steven TODO
 #template '/etc/nginx/sites-enabled/jenkins.conf' do
