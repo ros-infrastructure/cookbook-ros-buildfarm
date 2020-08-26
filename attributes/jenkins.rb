f2ea6c57553af851887a9b9a00cdb67de27153f0
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-08-26 16:31:13 -0400

    Add default server name attribute.

--- attributes/jenkins.rb
+++ attributes/jenkins.rb
@@ -1,2 +1,3 @@
 default['ros_buildfarm']['jenkins']['timezone'] = 'America/Los_Angeles'
 default['ros_buildfarm']['jenkins']['auth_strategy'] = 'default'
+default['ros_buildfarm']['server_name'] = 'ros_buildfarm'
