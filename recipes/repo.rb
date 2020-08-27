6c29bc7ceb634990da2311b1cf4b562e70195ac3
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-08-27 19:48:31 -0400

    Reorganize data bag to avoid templating item names.
    After looking at different ways to manage environments and data bags
    we're largely standardizing on putting keys for multiple environments in
    a single data bag item.

--- recipes/repo.rb
+++ recipes/repo.rb
@@ -2,7 +2,7 @@ package "reprepro"
 
 package "openssh-server"
 
-host_keys = data_bag_item("ros_buildfarm_host_private_keys", "repo.#{node.chef_environment}")
+host_keys = data_bag_item("ros_buildfarm_host_keys", "repo")[node.chef_environment]
 %w(dsa ecdsa ed25519 rsa).each do |type|
   file "/etc/ssh/ssh_host_#{type}_key" do
     content host_keys[type]["private"]
