a2d06dd3153dd250740de9984893193b689d117f
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-12-18 15:28:28 -0800

    Add the signing key for the ros_bootstrap repository.
    This adds the public signing key for the ROS bootstrap repository on
    repos.ros.org to this cookbook. Keyservers these days are unreliable
    enough that storing the key in the cookbook makes the most sense.

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -1,4 +1,5 @@
 default['ros_buildfarm']['apt_repos']['bootstrap_url'] = 'http://repos.ros.org/repos/ros_bootstrap'
+default['ros_buildfarm']['apt_repos']['bootstrap_signing_key_id'] = '8EDB2EF661FC880E'
 default['ros_buildfarm']['apt_repos']['component'] = 'main'
 default['ros_buildfarm']['apt_repos']['architectures'] = %w[i386 amd64 arm64 armhf source]
 default['ros_buildfarm']['apt_repos']['suites'] = %w[xenial bionic focal stretch buster]
