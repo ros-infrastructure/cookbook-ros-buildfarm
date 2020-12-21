9afa8e9ab475a4868e1ed7839f94699ccf5cbd3b
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-12-21 14:59:15 -0800

    Add tests for docker-registry cache usage.

--- test/integration/repo/repo_test.rb
+++ test/integration/repo/repo_test.rb
@@ -6,3 +6,17 @@
 describe package('openssh-server') do
   it { should be_installed }
 end
+
+describe service('docker-registry') do
+  it { should be_running }
+end
+
+describe command('docker info') do
+  its('stdout') {
+    should match(%r{Registry Mirrors:[[:space:]]+http://localhost:5000})
+  }
+end
+
+describe command('docker run hello-world') do
+  its('exit_status') { should eq 0 }
+end
