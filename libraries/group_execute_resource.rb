b73f644c5d208f33b145dd9da6bce0b401e57ac8
Scott K Logan <logans@cottsay.net>
2021-03-02 14:45:49 -0800

    Override 'command' for the group_execute resource
    This way we can assert that the 'command' value's type is a string
    instead of an array, which is also supported by the 'execute' resource
    but is not supported by 'group_execute'.

--- libraries/group_execute_resource.rb
+++ libraries/group_execute_resource.rb
@@ -3,6 +3,10 @@ class Chef
     class GroupExecute < Chef::Resource::Execute
       provides :group_execute, target_mode: true
 
+      def command(arg=nil)
+        set_or_return(:command, arg, :kind_of => String)
+      end
+
       def secondary_groups(arg=nil)
         set_or_return(:secondary_groups, arg, :kind_of => Array)
       end
