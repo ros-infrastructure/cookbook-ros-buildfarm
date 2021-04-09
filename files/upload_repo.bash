90591a9543375587e98bf7bf655eb48e9cbdc6ed
Scott K Logan <logans@cottsay.net>
2021-04-09 08:19:10 -0700

    Add support for RHEL repos to the upload script (#86)

--- files/upload_repo.bash
+++ files/upload_repo.bash
@@ -2,21 +2,33 @@
 
 case "$1" in
 	main)
-		repo=main
+		user=ftpsync
+		repo=ubuntu/main
 		key=ros-push_id
 		;;
 	testing)
-		repo=testing
+		user=ftpsync
+		repo=ubuntu/testing
 		key=ros-shadow-fixed-push_id
+		;;
+	rhel-main)
+		user=ros
+		repo=rhel/main
+		key=ros-rhel-push_id
+		;;
+	rhel-testing)
+		user=ros
+		repo=rhel/testing
+		key=ros-shadow-fixed-rhel-push_id
 		;;
 	*)
 		echo "There is no upload configuration for '$1'."
 		exit 1
 esac
 
-mkdir -p /var/repos/ubuntu/$repo/project/trace/
-date -u > /var/repos/ubuntu/$repo/project/trace/repositories.ros.org
-ssh -T -i $HOME/upload_triggers/$key ftpsync@ftp-osl.osuosl.org
+mkdir -p /var/repos/$repo/project/trace/
+date -u > /var/repos/$repo/project/trace/repositories.ros.org
+ssh -T -i $HOME/upload_triggers/$key $user@ftp-osl.osuosl.org
 exit_code=$?
 if [ $exit_code -eq 0 ]
 then
