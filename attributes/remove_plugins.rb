3d5370fa60f1d4bf7ad48f8310d5fa9212a21f95
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-04-23 15:32:38 -0700

    Update plugins to address security advisories.
    Plugins removed
    ---------------
    * analysis-core
        This plugin was actually removed earlier in PR #23 but it will still
        be present on systems which were initially provisioned before the
        plugin was yanked.
    * config-file-provider
        I'm the least sure about this one but as far as I can tell it is not
        a dependency of any used plugin and is not used by any jobs in
        ros_buildfarm. Plugin is up for adoption but does have an updated
        version with fixes for the security issues if it turns out we need
        it.
    * cvs
        ros_buildfarm (assessed via grep) does not have any support for cvs
        repositories. The CVS plugin was previously installed by default on
        new Jenkins installations and that may be how it ended up here.
    * icon-shim
        This plugin was added to backport functionality to earlier Jenkins
        instances (<= 1.576) and the plugin library itself has been merged
        into Jenkins 2.269. It can now be unambiguously safely removed.
    
    Plugins updated
    ---------------
    * extra-colums 1.20 -> 1.23
    * matrix-auth 2.6.5 -> 2.6.6
    * mercurial 2.4 -> 2.14
    * scm-api 2.6.3 -> 2.6.4
    
    Changelogs for updated packages were reviewed and found to be benign.
    Only packages with pending security advisories were updated.

---
+++ attributes/remove_plugins.rb
@@ -0,0 +1,9 @@
+# Plugins on this list will be removed on each chef run.
+# Adding plugins to this list requires a minor version bump.
+# Removing plugins from this list requires a major version bump.
+default['ros_buildfarm']['jenkins']['remove_plugins'] = %w[
+analysis-core
+config-file-provider
+cvs
+icon-shim
+]
