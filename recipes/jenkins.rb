e77180ffa98846164ee723e1ab3c2f10dc5ecf4f
Steven! Ragnarök <steven@nuclearsandwich.com>
2022-04-15 14:59:33 -0700

    Extract acme.sh installation into a recipe.
    Alternatively, we could make it a resource housed in the cookbook but
    this is fine for the time being.

--- recipes/jenkins.rb
+++ recipes/jenkins.rb
@@ -234,27 +234,7 @@ if node['ros_buildfarm']['letsencrypt_enabled']
     notifies :restart, 'service[nginx]', :immediately
   end
 
-  # Install acme.sh for certificate signing and renewal.  git is required for acme.sh's setup
-  package 'git'
-  execute 'git clone https://github.com/acmesh-official/acme.sh' do
-    cwd '/root'
-    not_if 'test -d /root/acme.sh'
-  end
-  execute 'acmesh-install' do
-    environment 'HOME' => '/root'
-    cwd '/root/acme.sh'
-    cmd = './acme.sh --install --home /root/.acme.sh'
-    cmd << " --accountemail #{node['ros_buildfarm']['letsencrypt_email']}" if node['ros_buildfarm']['letsencrypt_email']
-    command cmd
-    not_if 'test -x /root/.acme.sh/acme.sh'
-  end
-
-  cookbook_file "/root/cert-update-hook.sh" do
-    source "cert-update-hook.sh"
-    owner "root"
-    group "root"
-    mode "0700"
-  end
+  include_recipe "ros_buildfarm::acmesh"
 
   # Create Let's Encrypt signed cert if it has not already been done.
   execute 'acme-issue-cert' do
