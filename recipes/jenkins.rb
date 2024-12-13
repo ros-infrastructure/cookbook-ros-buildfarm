7fd2868fed6c879aca074b622064adb516392280
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2024-12-13 19:54:04 +0000

    Upgrade openjdk version 8->21
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- recipes/jenkins.rb
+++ recipes/jenkins.rb
@@ -93,7 +93,9 @@ node.default['jenkins']['executor']['protocol'] = 'http'
 # Remove plugins that were required previously but are not now.
 # Delete *.jpi files in plugin directory by filtering plugins to remove from a grep command
 plugin_remove_filter = node.default['ros_buildfarm']['jenkins']['remove_plugins'].map! {|e| "#{e}.jpi"}.join("|")
-execute "ls /var/lib/jenkins/plugins | grep -E \"#{plugin_remove_filter}\" | xargs rm" do
+execute "ls /var/lib/jenkins/plugins | grep -E \"#{plugin_remove_filter}\" | xargs -r rm" do
+  # If there are no plugins, then we don't need to remove anythi  ng
+  only_if { ::Dir.exist? '/var/lib/jenkins/plugins/' }
 end
 
 # Install plugins required to run ros_buildfarm.
