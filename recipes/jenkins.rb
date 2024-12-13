ba6884548b42ef889ff9e34ddb1c107cccd48b56
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2024-12-13 14:27:09 +0000

    Fix plugin_remove_filter undefined variable
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- recipes/jenkins.rb
+++ recipes/jenkins.rb
@@ -89,7 +89,7 @@ node.default['jenkins']['executor']['protocol'] = 'http'
 
 # Remove plugins that were required previously but are not now.
 # Delete *.jpi files in plugin directory by filtering plugins to remove from a grep command
-plugin_remove_filter = default['ros_buildfarm']['jenkins']['remove_plugins'].map! {|e| "#{e}.jpi"}.join("|")
+plugin_remove_filter = node.default['ros_buildfarm']['jenkins']['remove_plugins'].map! {|e| "#{e}.jpi"}.join("|")
 execute "ls /var/lib/jenkins/plugins | grep -E \"#{plugin_remove_filter}\" | xargs rm" do
 end
 # Install bundled publish-over-ssh plugin which was delisted from the Jenkins plugin server
