e904dee1c0bec73ca4656c453ecc4d42632130f8
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2025-01-30 10:43:05 -0500

    Move plugin removal to plugins recipe
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -21,3 +21,12 @@ execute 'install-jenkins-plugins' do
   user "jenkins"
   notifies :restart, 'service[jenkins]', :delayed
 end
+
+# Remove plugins that were required previously but are not now. By:
+# * Listing all plugins (.jpi files) in the plugin directories
+# * Delete the plugins (.jpi file) stated in ['jenkins']['remove_plugins']
+plugin_remove_filter = node.default['ros_buildfarm']['jenkins']['remove_plugins'].map! {|e| "#{e}.jpi"}.join("|")
+execute "ls /var/lib/jenkins/plugins | grep -E \"#{plugin_remove_filter}\" | xargs -r rm" do
+  # If there are no plugins, then we don't need to remove anything
+  only_if { ::Dir.exist? '/var/lib/jenkins/plugins/' }
+end
