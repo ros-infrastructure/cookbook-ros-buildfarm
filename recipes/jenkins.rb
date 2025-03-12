15d191b60762d623d7c5f0d5c087f98e1b84ce89
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-03-12 12:08:59 -0700

    Prevent needrestart from restarting jenkins.
    Jenkins is heavy enough that an inconvenient or unplanned restart is
    quite disruptive.

--- recipes/jenkins.rb
+++ recipes/jenkins.rb
@@ -34,6 +34,14 @@ apt_update
 jdk_version = node.default['jenkins']['master']['jdk_version']
 package "openjdk-#{jdk_version}-jdk-headless"
 
+ruby_block "needrestart-config-jenkins" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/needrestart/needrestart.conf")
+    file.insert_line_if_no_match(%r[\$nrconf\{override_rc\}\{qr\(\^jenkins\\\.service\$\)\} = 0;], %q[$nrconf{override_rc}{qr(^jenkins\.service$)} = 0;])
+    file.write_file
+  end
+end
+
 # Jenkins downgrade protection
 #
 # The Jenkins package has transitioned to using systemd units instead of
