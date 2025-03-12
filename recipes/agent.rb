8e1722d0db804b6ae1950b835bf8c9a052e2c91e
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-03-12 11:53:52 -0700

    Add jenkins-agent to needrestart overrides.
    needrestart in 24.04 now runs for both attended and unattended upgrades.
    It has a built-in list of long-running services which should not be
    restarted arbitrarily but need to include our jenkins-agent since it may
    be running jobs and shouldn't be stopped by an unattended-upgrade run.

--- recipes/agent.rb
+++ recipes/agent.rb
@@ -161,6 +161,14 @@ file '/etc/jenkins-agent/token' do
   group agent_username
 end
 
+ruby_block "needrestart-config" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/needrestart/needrestart.conf")
+    file.insert_line_if_no_match(%r[\$nrconf\{override_rc\}\{qr\(\^jenkins-agent\\\.service\$\)\} = 0;], %q[$nrconf{override_rc}{qr(^jenkins-agent\.service$)} = 0;])
+    file.write_file
+  end
+end
+
 template '/etc/systemd/system/jenkins-agent.service' do
   source 'jenkins-agent.service.erb'
   variables Hash[
