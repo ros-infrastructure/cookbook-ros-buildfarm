ff16c631d799226b90a202474762ffa0d6bd23d5
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-05-15 17:57:05 -0700

    Allow more restart attempts.
    We restart jenkins fairly frequently during provisioning and dokken
    tests were failing to keep up.

--- recipes/jenkins.rb
+++ recipes/jenkins.rb
@@ -96,6 +96,11 @@ include_recipe 'jenkins::jenkins'
 
 # Increase timeout of jenkins systemd unit
 # Timeout extension prevents Jenkins startup failures due to slow init scripts execution
+execute "systemctl-daemon-reload" do
+  command "systemctl daemon-reload"
+  action :nothing
+end
+
 directory '/etc/systemd/system/jenkins.service.d' do
   mode '0755'
   owner 'root'
@@ -106,11 +111,9 @@ cookbook_file '/etc/systemd/system/jenkins.service.d/500-timeout.conf' do
   source 'jenkins/service/500-timeout.conf'
   owner 'root'
   group 'root'
+  notifies :run, 'execute[systemctl-daemon-reload]', :immediately
 end
 
-execute "systemctl-daemon-reload" do
-  command "systemctl daemon-reload"
-end
 
 # Set up authentication
 chef_user = search('ros_buildfarm_jenkins_users', 'chef_user:true').first
