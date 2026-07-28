bd40760af3590794b25656865958b542efef861e
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2026-07-28 17:11:05 -0500

    Clean comment
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- attributes/jenkins.rb
+++ attributes/jenkins.rb
@@ -20,8 +20,7 @@ default['ros_buildfarm']['letsencrypt_enabled'] = false
 # When set to true a postfix SMTP server will be configured for use by Jenkins via sendmail.
 default['ros_buildfarm']['smtp'] = false
 
-# Last version supporting Java 8
-#default['jenkins']['master']['version'] = '2.346.1'
+# LTS version of Jenkins to install (https://www.jenkins.io/changelog-stable/)
 default['jenkins']['master']['version'] = '2.555.3'
 
 # JDK version to install
